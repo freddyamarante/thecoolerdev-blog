388010558190d54d6c529ac6212aaf90c539c7b6
Freddy Amarante <freddyamarante14@gmail.com>
2023-05-26 16:47:27 -0400

    fix: hidration missmatch warning

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { Button } from '@/stories/Button'
+
 export default function Home() {
+  const saySomething = () => console.log('ayo')
   return (
-    <main className="text-8xl font-extrabold">
-      <h1>Moises Ortega</h1>
-    </main>
+    <>
+      <main className="text-8xl font-extrabold">
+        <h1>Moises Ortega</h1>
+      </main>
+      <Button label="Dark mode toggler" onClick={saySomething} />
+    </>
   )
 }
