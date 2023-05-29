9b3bce80629c95ff87569a7bb43fbbfb265aaf49
Freddy Amarante <freddyamarante14@gmail.com>
2023-05-28 22:03:46 -0400

    feat: create get posts function and basic test

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,23 @@
-'use client'
-
-import { Button } from '@/stories/components/Button'
+import { getPosts } from '@/sanity/sanity-utils'
 import DarkModeToggler from '@/stories/components/DarkModeToggler'
+import { PortableText } from '@portabletext/react'
 
-export default function Home() {
-  const saySomething = () => console.log('ayo')
+export default async function Home() {
+  const posts = await getPosts()
   return (
     <div className="flex justify-center flex-col">
       <main className="text-8xl font-extrabold">
         <h1 className="text-night dark:text-cloud">Moises Ortega</h1>
       </main>
-      <Button
-        primary
-        size="large"
-        label="Just a regular button"
-        onClick={saySomething}
-      />
       <DarkModeToggler />
+      <div className="mt-5 grid grid-cols-3 gap-8">
+        {posts.map((post) => (
+          <>
+            <div>{post.title}</div>
+            <PortableText value={post.body} />
+          </>
+        ))}
+      </div>
     </div>
   )
 }
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,3 +1,18 @@
 import { createClient, groq } from 'next-sanity'
 import { Post } from '@/types/Post'
 import config from './config/config'
+
+export async function getPosts(): Promise<Post[]> {
+  return createClient(config).fetch(
+    groq`*[_type == "post"]{
+      _id,
+      title,
+      "slug": slug.current,
+      author->,
+      "mainImage": mainImage.asset->url,
+      publishedAt,
+      categories[]->,
+      body
+    }`
+  )
+}
