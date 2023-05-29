6faadff75e7f024b66405175c7f319112eb9ad2c
Freddy Amarante <freddyamarante14@gmail.com>
2023-05-29 18:22:17 -0400

    feat: setup folder management for appDir

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import DarkModeToggler from '@/stories/components/DarkModeToggler'
+
+export default async function Home() {
+  return (
+    <div className="flex justify-center flex-col">
+      <main className="text-8xl font-extrabold">
+        <h1 className="text-night dark:text-cloud">Moises Ortega</h1>
+      </main>
+      <DarkModeToggler />
+    </div>
+  )
+}
