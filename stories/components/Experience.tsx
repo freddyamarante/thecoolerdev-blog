2ca41f6cb256ee6dc7ef28184e7398fdbd79c703
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-05 16:30:53 -0400

    feat: set basic shaped position

--- stories/components/Experience.tsx
+++ stories/components/Experience.tsx
@@ -1,25 +1,13 @@
 'use client'
 
 import {
-  CameraShake,
   Float,
   Torus,
   RoundedBox,
   Sphere,
-  Cone,
+  Dodecahedron,
 } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
-
-const cameraConfig = {
-  maxYaw: 0.005,
-  maxPitch: 0.5,
-  maxRoll: 0.5,
-  yawFrequency: 0.1,
-  pitchFrequency: 0.1,
-  rollFrequency: 0.1,
-  intensity: 0.3,
-  decayRate: 0.65,
-}
 
 interface ExperienceProps {
   className?: React.ComponentProps<'div'>['className']
@@ -29,7 +17,6 @@ const Experience = ({ className }: ExperienceProps) => {
   return (
     <div className={className}>
       <Canvas>
-        <CameraShake {...cameraConfig} />
         <ambientLight intensity={0.38} color="#f5cb5c" />
         <directionalLight intensity={5} color="#f5cb5c" />
         <directionalLight
@@ -38,7 +25,7 @@ const Experience = ({ className }: ExperienceProps) => {
           color="#E8EDDF"
         />
 
-        <Float rotationIntensity={2} position={[0, 0, 0]}>
+        <Float rotationIntensity={2} position={[6, 3, 0]}>
           <RoundedBox
             args={[2, 2, 2]}
             radius={0.2}
@@ -49,22 +36,22 @@ const Experience = ({ className }: ExperienceProps) => {
           </RoundedBox>
         </Float>
 
-        <Float rotationIntensity={2} position={[3, 0, 0]}>
+        <Float rotationIntensity={2} position={[-6, 3, 0]}>
           <Torus args={[1, 0.6, 16]}>
             <meshPhongMaterial color="#242423" />
           </Torus>
         </Float>
 
-        <Float rotationIntensity={2} position={[-3, 0, 0]}>
+        <Float rotationIntensity={2} position={[6, -3, 0]}>
           <Sphere args={[1.1]}>
             <meshPhongMaterial color="#242423" />
           </Sphere>
         </Float>
 
-        <Float rotationIntensity={2} position={[0, 3, 0]}>
-          <Cone args={[1, 1.5]}>
+        <Float rotationIntensity={2} position={[-6, -3, 0]}>
+          <Dodecahedron args={[1.4]}>
             <meshPhongMaterial color="#242423" />
-          </Cone>
+          </Dodecahedron>
         </Float>
       </Canvas>
     </div>
