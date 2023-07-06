181184bc20ea0ba96bd53706fd85b956e50a2b8f
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-06 19:41:34 -0400

    feat: set environment variables

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,13 +1,12 @@
 import { createClient } from '@sanity/client'
 import groq from 'groq'
 
-import config from './config/config'
 import { Post } from '@/types/Post'
 import { LandingPage } from '@/types/LandingPage'
 
-const projectId = config.projectId
-const dataset = config.dataset
-const apiVersion = config.apiVersion
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
 const client = createClient({
   projectId,
