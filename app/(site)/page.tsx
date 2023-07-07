062fee8c1f4e51e54cb43f18871f34cfaa5d5bb9
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-07 15:22:33 -0400

    feat: separate components and resources in stories folders

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,8 @@
 import GradientBackground from '@/stories/components/GradientBackground'
-import Experience from '@/stories/components/Experience'
+import Experience from '@/stories/resources/Experience'
 
 import '@/styles/globals.css'
-import GrainOverlay from '@/stories/components/GrainOverlay'
+import GrainOverlay from '@/stories/resources/GrainOverlay'
 
 export default async function Home() {
   return (
