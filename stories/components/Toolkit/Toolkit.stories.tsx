707415d8d17f0373bec675220773adba92fbde9d
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-13 20:25:44 -0400

    feat: create toolkit component

--- stories/components/Toolkit/Toolkit.stories.tsx
+++ stories/components/Toolkit/Toolkit.stories.tsx
@@ -5,10 +5,46 @@ import Toolkit from './Toolkit'
 const meta: Meta<typeof Toolkit> = {
   title: 'Toolkit/Toolkit',
   component: Toolkit,
+  decorators: [
+    (Story) => (
+      <div className="flex justify-center bg-taxi p-16 overflow-x-hidden">
+        <Story />
+      </div>
+    ),
+  ],
   tags: ['autodocs'],
 }
 
 export default meta
 type Story = StoryObj<typeof Toolkit>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    subtitle: 'My toolkit',
+    subBody:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    button: 'Learn more',
+    tools: [
+      {
+        title: 'JavaScript',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: null,
+      },
+      {
+        title: 'React',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: null,
+      },
+      {
+        title: 'Tailwind',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: null,
+      },
+      {
+        title: 'TypeScript',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        image: null,
+      },
+    ],
+  },
+}
--- stories/components/Toolkit/Toolkit.tsx
+++ stories/components/Toolkit/Toolkit.tsx
@@ -1,7 +1,45 @@
-interface Toolkit {}
+import Button from '../Button'
+import Tool from './Tool'
 
-const Toolkit = () => {
-  return <></>
+interface ToolkitProps {
+  subtitle?: string
+  subBody?: string
+  button?: string
+  tools?: {
+    title: string
+    description: string
+    image: string | null
+  }[]
+}
+
+const Toolkit = ({
+  subtitle,
+  subBody,
+  button = 'Learn more',
+  tools = [],
+}: ToolkitProps) => {
+  return (
+    <div className="w-full px-12 py-16 rounded-xl bg-night text-white">
+      <div className="flex items-center">
+        <div className="flex flex-col gap-8 pr-4 flex-shrink-0 basis-2/5">
+          <div className="text-4xl font-bold leading-normal">{subtitle}</div>
+          <div className="text-2xl">{subBody}</div>
+          <div className="flex justify-start">
+            <Button mode="primary" label={button} />
+          </div>
+        </div>
+        <div className="flex justify-start items-center gap-8 ml-20 text-night">
+          {tools.map((tool, index) => (
+            <Tool
+              title={tool.title}
+              description={tool.description}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Toolkit
