0960cb2b17ef00501a4f6bbb00fc094aca21d5bb
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-14 00:21:05 -0400

    fix: dynamic drag constraints

--- stories/components/Toolkit/Tool.tsx
+++ stories/components/Toolkit/Tool.tsx
@@ -11,7 +11,7 @@ const defaultImage =
 
 const Tool = ({ title = 'Framework', description, image }: ToolProps) => {
   return (
-    <div className="flex flex-row w-[440px] h-[275px] px-8 justify-center items-center rounded-2xl bg-cloud-dark">
+    <div className="relative flex flex-row w-[440px] h-[275px] px-8 justify-center items-center rounded-2xl bg-cloud-dark z-10">
       <div className="flex flex-col gap-2 grow-0">
         <span className="text-3xl font-bold">{title}</span>
         <span className="text-xl">{description}</span>
--- stories/components/Toolkit/Toolkit.tsx
+++ stories/components/Toolkit/Toolkit.tsx
@@ -1,5 +1,10 @@
+'use client'
+
+import { useMotionValue, useTransform, motion } from 'framer-motion'
 import Button from '../Button'
 import Tool from './Tool'
+
+import { useRef, useLayoutEffect, useState } from 'react'
 
 interface ToolkitProps {
   subtitle?: string
@@ -18,24 +23,65 @@ const Toolkit = ({
   button = 'Learn more',
   tools = [],
 }: ToolkitProps) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const infoRef = useRef<HTMLDivElement>(null)
+  const boxRef = useRef<HTMLDivElement>(null)
+
+  const [boxWidth, setBoxWidth] = useState<number>(0)
+  const [offset, setOffset] = useState<number>(0)
+
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      if (boxRef.current)
+        setBoxWidth(boxRef.current.getBoundingClientRect().width)
+
+      if (containerRef.current && infoRef.current)
+        setOffset(
+          containerRef.current.getBoundingClientRect().width -
+            infoRef.current.getBoundingClientRect().width
+        )
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div className="w-full px-12 py-16 rounded-xl bg-night text-white">
+    <div
+      className="w-full px-12 py-16 rounded-xl bg-night text-white"
+      ref={containerRef}
+    >
       <div className="flex items-center">
-        <div className="flex flex-col gap-8 pr-4 flex-shrink-0 basis-2/5">
+        <div
+          className="flex flex-col gap-8 pr-4 flex-shrink-0 basis-2/5"
+          ref={infoRef}
+        >
           <div className="text-4xl font-bold leading-normal">{subtitle}</div>
           <div className="text-2xl">{subBody}</div>
           <div className="flex justify-start">
             <Button mode="primary" label={button} />
           </div>
         </div>
-        <div className="flex justify-start items-center gap-8 ml-20 text-night">
-          {tools.map((tool, index) => (
-            <Tool
-              title={tool.title}
-              description={tool.description}
-              key={index}
-            />
-          ))}
+        <div className="flex">
+          <div className="relative top-0 overflow-hidden" ref={boxRef}>
+            <motion.div
+              drag="x"
+              dragConstraints={{ left: -boxWidth + offset - 96, right: 0 }}
+              className="flex items-center gap-8 ml-20 text-night"
+            >
+              {tools.map((tool, index) => (
+                <Tool
+                  title={tool.title}
+                  description={tool.description}
+                  key={index}
+                />
+              ))}
+            </motion.div>
+          </div>
         </div>
       </div>
     </div>
