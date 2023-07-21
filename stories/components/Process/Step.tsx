d651a6e7ee6007372897e5f79e072015f22bb363
Freddy Amarante <freddyamarante14@gmail.com>
2023-07-21 18:19:25 -0400

    feat: remove disclosure component from headless ui

--- stories/components/Process/Step.tsx
+++ stories/components/Process/Step.tsx
@@ -1,4 +1,3 @@
-import { Disclosure } from '@headlessui/react'
 import { ChevronDown, ChevronUp } from '../resources/Icons'
 
 interface StepProps {
@@ -17,33 +16,31 @@ const Step = ({
   onToggle,
 }: StepProps) => {
   return (
-    <Disclosure as="nav" className="bg-night w-full rounded-xl px-3">
-      {({ open }) => (
-        <Disclosure.Button
-          className="flex flex-col justify-center w-full"
-          onClick={onToggle}
-        >
-          <div className="flex flex-row justify-between items-center w-full">
-            <div className="flex flex-row text-4xl font-medium text-white leading-none gap-x-9">
-              <div>{`0${(number + 1).toString()}`}</div>
-              <div>{title}</div>
-            </div>
-            <div className="flex rounded-2xl p-2 my-3 text-white hover:bg-night-light focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-              <span className="sr-only">More information</span>
-              {isOpen ? (
-                <ChevronUp aria-hidden="true" />
-              ) : (
-                <ChevronDown aria-hidden="true" />
-              )}
-            </div>
+    <div className="bg-night w-full rounded-xl px-3">
+      <div
+        className="flex flex-col justify-center w-full cursor-pointer"
+        onClick={onToggle}
+      >
+        <div className="flex flex-row justify-between items-center w-full">
+          <div className="flex flex-row text-4xl font-medium text-white leading-none gap-x-9">
+            <div>{`0${(number + 1).toString()}`}</div>
+            <div>{title}</div>
           </div>
+          <div className="flex rounded-2xl p-2 my-3 text-white hover:bg-night-light focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span className="sr-only">More information</span>
+            {isOpen ? (
+              <ChevronUp aria-hidden="true" />
+            ) : (
+              <ChevronDown aria-hidden="true" />
+            )}
+          </div>
+        </div>
 
-          <Disclosure.Panel className="px-4 py-6 rounded-2xl text-white">
-            {description}
-          </Disclosure.Panel>
-        </Disclosure.Button>
-      )}
-    </Disclosure>
+        {isOpen && (
+          <div className="px-4 py-6 rounded-2xl text-white">{description}</div>
+        )}
+      </div>
+    </div>
   )
 }
 
