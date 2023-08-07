4d2697403e03177154cbe0b6c0951d90b387b940
freddy <freddyamarante14@gmail.com>
2023-08-07 00:12:05 -0400

    fix: cols layout

--- stories/components/Blog/Posts.tsx
+++ stories/components/Blog/Posts.tsx
@@ -36,7 +36,7 @@ const Posts = ({ title, description, button, posts = [] }: PostsProps) => {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 lg:gap-5">
         <GradientBackground
           backgroundColor="#CFDBD5"
           gradientColor="#A5A5B6"
