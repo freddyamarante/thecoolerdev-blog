6b94147482792ccda8bd1efd21dffb7e2118aa15
freddy <freddyamarante14@gmail.com>
2023-08-10 15:23:44 -0400

    feat: style dark mode toggler

--- stories/components/DarkModeToggler.tsx
+++ stories/components/DarkModeToggler.tsx
@@ -1,5 +1,8 @@
 'use client'
+
+import { motion } from 'framer-motion'
 import { useTheme } from 'next-themes'
+
 import { Moon, Sun } from './resources/Icons'
 
 const DarkModeToggler = () => {
@@ -12,13 +15,16 @@ const DarkModeToggler = () => {
   }
 
   return (
-    <button
+    <motion.button
+      initial={{ scale: 1 }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
       type="button"
       onClick={toggleDarkMode}
-      className="flex items-center justify-center h-8 w-8 rounded-full font-bold dark:bg-cloud bg-night dark:text-night text-cloud"
+      className="flex items-center justify-center h-8 w-8 rounded-full font-bold border-2 border-night-dark dark:border-white dark:text-white text-night-dark"
     >
       {currentTheme === 'dark' ? <Moon /> : <Sun />}
-    </button>
+    </motion.button>
   )
 }
 
