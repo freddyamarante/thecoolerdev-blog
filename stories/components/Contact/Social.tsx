b6a59298b8a4cc002efa2079c5f054afe7a69c95
freddy <freddyamarante14@gmail.com>
2023-08-09 23:47:33 -0400

    feat: add styling to socials

--- stories/components/Contact/Social.tsx
+++ stories/components/Contact/Social.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { motion } from 'framer-motion'
+
 import { CV, Email, Github, LinkedIn, Phone, Twitter } from '../resources/Icons'
 
 interface SocialProps {
@@ -33,14 +35,17 @@ const Social = ({ icon, link = '', label }: SocialProps) => {
   }
 
   return (
-    <a
+    <motion.a
       href={link}
+      initial={{ scale: 1 }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
       target={icon === 'email' || icon === 'phone' ? '' : '_blank'}
-      className="flex flex-row justify-between items-center gap-2 border-2 border-night bg-transparent py-3 px-5 w-full rounded-2xl cursor-pointer"
+      className="flex flex-row justify-between items-center gap-2 border-2 border-night bg-transparent hover:bg-violet py-3 px-5 w-full rounded-2xl cursor-pointer"
     >
       <span className="font-semibold text-xl md:text-2xl">{label}</span>
       {chosenIcon}
-    </a>
+    </motion.a>
   )
 }
 
