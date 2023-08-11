cb27bc8ebcea05fd8ee3a03b66c6c6347ec055ac
freddy <freddyamarante14@gmail.com>
2023-08-11 17:16:00 -0400

    feat: add posts to blog page

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,8 +1,12 @@
+import { getPosts } from '@/sanity/sanity-utils'
 import GoBack from '@/stories/components/Blog/GoBack'
+import Posts from '@/stories/components/Blog/Posts'
 
 import Link from 'next/link'
 
-const Blog = () => {
+const Blog = async () => {
+  const posts = await getPosts()
+
   return (
     <div className="overflow-hidden bg-cloud">
       <main className="px-2 sm:px-6 lg:px-10 py-8">
@@ -11,6 +15,10 @@ const Blog = () => {
             <GoBack />
           </Link>
         </div>
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-center mb-8">
+          Read my blog
+        </h1>
+        <Posts posts={posts} evenOddLayout />
       </main>
     </div>
   )
