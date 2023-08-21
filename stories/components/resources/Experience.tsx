670b4a9787c456e49945ca73e771606d79f3ffa5
freddy <freddyamarante14@gmail.com>
2023-08-20 22:55:05 -0400

    feat: add responsiveness to experience

--- stories/components/resources/Experience.tsx
+++ stories/components/resources/Experience.tsx
@@ -1,58 +1,59 @@
 'use client'
 
-import {
-  Float,
-  Torus,
-  RoundedBox,
-  Sphere,
-  Dodecahedron,
-} from '@react-three/drei'
+import * as THREE from 'three'
+import { Float } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useEffect, useState } from 'react'
 
 type ExperienceProps = {
   className?: React.ComponentProps<'div'>['className']
 }
 
+const geometry = new THREE.DodecahedronGeometry(1.5)
+const material = new THREE.MeshPhongMaterial({ color: '#242423' })
+
 const Experience = ({ className }: ExperienceProps) => {
+  const [screenWidth, setScreenWidth] = useState(0)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth)
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
     <div className={className}>
       <Canvas>
         <ambientLight intensity={0.38} color="#f5cb5c" />
-        <directionalLight intensity={5} color="#f5cb5c" />
-        <directionalLight
-          position={[0, -5, 2]}
-          intensity={0.5}
-          color="#E8EDDF"
-        />
+        <pointLight position={[0, 0, 3]} intensity={3} color="#f5cb5c" />
 
-        <Float rotationIntensity={2} position={[6, 3, 0]}>
-          <RoundedBox
-            args={[2, 2, 2]}
-            radius={0.2}
-            smoothness={4}
-            creaseAngle={0.4}
-          >
-            <meshPhongMaterial color="#242423" />
-          </RoundedBox>
-        </Float>
+        {screenWidth > 920 ? (
+          <>
+            <Float rotationIntensity={2} position={[6, 3, 0]}>
+              <mesh geometry={geometry} material={material} />
+            </Float>
 
-        <Float rotationIntensity={2} position={[-6, 3, 0]}>
-          <Torus args={[1, 0.6, 16]}>
-            <meshPhongMaterial color="#242423" />
-          </Torus>
-        </Float>
+            <Float rotationIntensity={2} position={[-6, 3, 0]}>
+              <mesh geometry={geometry} material={material} />
+            </Float>
 
-        <Float rotationIntensity={2} position={[6, -3, 0]}>
-          <Sphere args={[1.1]}>
-            <meshPhongMaterial color="#242423" />
-          </Sphere>
-        </Float>
+            <Float rotationIntensity={2} position={[6, -3, 0]}>
+              <mesh geometry={geometry} material={material} />
+            </Float>
 
-        <Float rotationIntensity={2} position={[-6, -3, 0]}>
-          <Dodecahedron args={[1.4]}>
-            <meshPhongMaterial color="#242423" />
-          </Dodecahedron>
-        </Float>
+            <Float rotationIntensity={2} position={[-6, -3, 0]}>
+              <mesh geometry={geometry} material={material} />
+            </Float>
+          </>
+        ) : null}
       </Canvas>
     </div>
   )
