d25f6685d693c76548c799d12af372e78d4310a3
Freddy Amarante <freddyamarante14@gmail.com>
2023-08-24 21:54:03 -0400

    fix: responsive design on tools

--- stories/components/Toolkit/Tool.tsx
+++ stories/components/Toolkit/Tool.tsx
@@ -10,14 +10,14 @@ const defaultImage = '/javascriptImage.png'
 
 const Tool = ({ title = 'Framework', description, image }: ToolProps) => {
   return (
-    <div className="relative flex flex-row w-[290px] sm:w-[380px] h-[180px] sm:h-[220px] lg:w-[440px] lg:h-[260px] px-6 lg:px-8 justify-center items-center rounded-2xl bg-cloud-dark z-10">
+    <div className="relative flex flex-row w-[290px] sm:w-[380px] h-[180px] sm:h-[220px] lg:w-[440px] lg:h-[260px] px-4 sm:px-6 lg:px-8 justify-center items-center rounded-2xl bg-cloud-dark z-10">
       <div className="flex flex-col gap-2 grow-0">
         <span className="text-xl sm:text-2xl lg:text-3xl font-bold">
           {title}
         </span>
-        <span className="text-md sm:text-lg lg:text-xl">{description}</span>
+        <span className="text-sm sm:text-lg lg:text-xl">{description}</span>
       </div>
-      <div className="flex w-24 h-24 lg:w-32 lg:h-32 ml-8 flex-shrink-0 items-center">
+      <div className="flex w-16 h-16 sm:w-24 sm:h-24 lg:w-32 lg:h-32 ml-2 lg:ml-8 flex-shrink-0 items-center">
         <Image
           src={image ? image : defaultImage}
           alt={title}
