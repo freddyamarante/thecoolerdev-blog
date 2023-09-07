8a62f7a414a47632d064210858258e78ba24e3b2
Freddy Amarante <freddyamarante14@gmail.com>
2023-09-07 01:34:25 -0400

    feat: add code/input

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { schemaTypes } from './sanity/schemas'
 import { visionTool } from '@sanity/vision'
+import { codeInput } from '@sanity/code-input'
 import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 const config = defineConfig({
@@ -12,7 +13,7 @@ const config = defineConfig({
   useCdn: false,
 
   basePath: '/admin',
-  plugins: [deskTool(), visionTool(), vercelDeployTool()],
+  plugins: [deskTool(), visionTool(), vercelDeployTool(), codeInput()],
 
   apiVersion: '2023-05-06',
   schema: {
--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -22,22 +22,22 @@ const blockContent = {
       // you want and decide how you want to deal with it where you want to
       // use your content.
       styles: [
-        {title: 'Normal', value: 'normal'},
-        {title: 'H1', value: 'h1'},
-        {title: 'H2', value: 'h2'},
-        {title: 'H3', value: 'h3'},
-        {title: 'H4', value: 'h4'},
-        {title: 'Quote', value: 'blockquote'},
-        {title: 'Code', value: 'code'}
+        { title: 'Normal', value: 'normal' },
+        { title: 'H1', value: 'h1' },
+        { title: 'H2', value: 'h2' },
+        { title: 'H3', value: 'h3' },
+        { title: 'H4', value: 'h4' },
+        { title: 'Quote', value: 'blockquote' },
+        { title: 'Code', value: 'code' },
       ],
-      lists: [{title: 'Bullet', value: 'bullet'}],
+      lists: [{ title: 'Bullet', value: 'bullet' }],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
         // preference or highlighting by editors.
         decorators: [
-          {title: 'Strong', value: 'strong'},
-          {title: 'Emphasis', value: 'em'},
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [
@@ -61,7 +61,17 @@ const blockContent = {
     // as a block type.
     {
       type: 'image',
-      options: {hotspot: true},
+      options: { hotspot: true },
+    },
+    {
+      type: 'code',
+      language: 'javascript',
+      languageAlternatives: [
+        { title: 'Javascript', value: 'javascript' },
+        { title: 'HTML', value: 'html' },
+        { title: 'CSS', value: 'css' },
+        { title: 'JSON', value: 'json' },
+      ],
     },
   ],
 }
