4aaa9e3aab8bc805440dc1687ee2a20f0aa9e566
Freddy Amarante <freddyamarante14@gmail.com>
2023-09-07 00:39:25 -0400

    feat: revert to sanity config without gitignore

---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './sanity/schemas'
+import { visionTool } from '@sanity/vision'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
+
+const config = defineConfig({
+  title: 'thecoolerdev-blog-studio',
+
+  projectId: '7eiqcloi',
+  dataset: 'production',
+  useCdn: false,
+
+  basePath: '/admin',
+  plugins: [deskTool(), visionTool(), vercelDeployTool()],
+
+  apiVersion: '2023-05-06',
+  schema: {
+    types: schemaTypes,
+  },
+})
+
+export default config
